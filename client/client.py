fbeaa49828bc96dcb5ffc8460be5c1646398043e
Waqar <ktk312@gmail.com>
2025-10-06 10:23:46 +0500

    Update client.py with timeout

--- client/client.py
+++ client/client.py
@@ -1,20 +1,26 @@
 import requests
 
-BASE_URL = "https://rpc-server-assignment-production.up.railway.app"   
+BASE_URL = "https://rpc-server-assignment-production.up.railway.app"
 
 
-def add(x: int, y: int) -> dict:  
+def add(x: int, y: int) -> dict:
     payload = {"x": x, "y": y}
-    response = requests.post(f"{BASE_URL}/add", json=payload)
-    response.raise_for_status()  
-    return response.json()
+    try:
+        response = requests.post(f"{BASE_URL}/add", json=payload, timeout=2)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.Timeout:
+        return {"error": "Request timed out"}
 
 
 def multiply(x: int, y: int) -> dict:
     payload = {"x": x, "y": y}
-    response = requests.post(f"{BASE_URL}/multiply", json=payload)
-    response.raise_for_status()
-    return response.json()
+    try:
+        response = requests.post(f"{BASE_URL}/multiply", json=payload, timeout=2)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.Timeout:
+        return {"error": "Request timed out"}
 
 
 if __name__ == "__main__":
